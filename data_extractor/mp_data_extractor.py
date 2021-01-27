89ffb4414f7cd45bda1d29162b48079b5d1b455c
Tim Atkin <ttatkin@gmail.com>
2021-01-27 22:10:25 +1300

    Update gitignore and started on bill extraction

--- data_extractor/mp_data_extractor.py
+++ data_extractor/mp_data_extractor.py
@@ -1,8 +1,26 @@
 import glob, os
 from bs4 import BeautifulSoup
 os.chdir("./saved_pages/current_mps/")
+
+list_count = 0
+
 for file in glob.glob("*.html"):
-    print(file)
+    #print(file)
     with open(file, "r", encoding="utf8") as f:
       soup = BeautifulSoup(f.read(), 'html.parser')
-      
+      # h1 rule is same for current and former
+      title_name = soup.h1.get_text().strip()
+      seat_text = soup.find_all("h2")[0].get_text()
+      list_member = seat_text.lower().find("list member") >= 0
+      party = seat_text.split(",")[1].strip()
+      if (list_member):
+        electorate = ""
+      else:
+        electorate = seat_text.split(",")[0][11:].strip()
+
+      factoids = soup.select("#main-content .body-text ul")[1]
+      #main-content .body-text ul
+
+      print(f"{title_name}:\n{factoids.get_text()}")
+
+print(list_count)
--- data_extractor/parliament_page_downloader.py
+++ data_extractor/parliament_page_downloader.py
@@ -1,8 +1,35 @@
 import time
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import math
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+def save_bill_pages():
+  URL = "https://www.parliament.nz/en/pb/bills-and-laws/bills-proposed-laws/all?Criteria.PageNumber=1"
+  page = requests.get(URL, headers=headers)
+  soup = BeautifulSoup(page.content, 'html.parser')
+
+  # First need to get the total results to work out the number of pages
+  # Should say "Displaying 2401 - 2442 of 2442" so we just need the last number
+  total_results = int(soup.find("span", {"class":"listing-result"}).get_text().split("of")[-1].strip())
+  page_count = math.floor(total_results/50+2)
+
+  for page in range(1, page_count):
+    URL = f"https://www.parliament.nz/en/pb/bills-and-laws/bills-proposed-laws/all?Criteria.PageNumber={page}"
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    rows = soup.find("table").find("tbody").find_all("tr")
+    for row in rows:
+      link = row.find_all("a")[0]
+      updated_date = datetime.strptime(row.find_all("td")[3].get_text().strip(), "%d %B %Y")
+      
+      output_file = f"./saved_pages/bills/{link['href'].split('/')[-2]}_{updated_date.strftime('%Y-%m-%d')}.html"
+      print(output_file)
+      bill_page = requests.get("https://www.parliament.nz"+link["href"], headers=headers)
+      time.sleep(0.5)
+      open(output_file, 'wb').write(bill_page.content)
 
 def save_current_mp_pages():
   URL = "https://www.parliament.nz/en/mps-and-electorates/members-of-parliament/"
@@ -30,4 +57,4 @@ def save_previous_mp_pages():
     print(output_file)
     open(output_file, 'wb').write(mp_page.content)
 
-save_previous_mp_pages()
+save_bill_pages()
